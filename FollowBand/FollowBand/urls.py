b389c77c7f45808bdd41e06063da57977bdb42be
tprzyluc <tymoteusz.przylucki@gmail.com>
2018-10-21 12:17:41 +0200

    Include URLconf

--- FollowBand/FollowBand/urls.py
+++ FollowBand/FollowBand/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from . import views
-from homepage import templates
+
 
 urlpatterns = [
     re_path('^$', views.start),
